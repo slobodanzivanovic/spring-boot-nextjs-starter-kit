8af64d7f267d6cbf7cb59db311e3eb18d119959c
Slobodan Zivanovic <207845948+slobodanzivanovic@users.noreply.github.com>
2025-04-18 02:32:40 +0200

    Fixed unable to access jarfile error

--- backend/core/src/main/java/com/slobodanzivanovic/example/core/CoreApplication.java
+++ backend/core/src/main/java/com/slobodanzivanovic/example/core/CoreApplication.java
@@ -1,13 +1,19 @@
 package com.slobodanzivanovic.example.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.slobodanzivanovic.example")
 public class CoreApplication {
 
+	private static final String CLASSNAME = CoreApplication.class.getName();
+	private static final Logger LOGGER = LoggerFactory.getLogger(CoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+		LOGGER.info("{} started", CLASSNAME);
 	}
 
 }
