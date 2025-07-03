09eaf6494eb6554beb90a688b399c1c0031834fc
Slobodan Zivanovic <219014701+slobodanzivanovic@users.noreply.github.com>
2025-07-03 15:40:36 +0200

    Created Base entity class

---
+++ backend/src/main/java/com/slobodanzivanovic/backend/model/common/entity/BaseEntity.java
@@ -0,0 +1,90 @@
+package com.slobodanzivanovic.backend.model.common.entity;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Base abstract entity class providing common fields for all db entities.
+ *
+ * @author Slobodan Zivanovic
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(of = "id")
+public class BaseEntity implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@CreatedBy
+	@Column(name = "creaated_by", updatable = false)
+	private String createdBy;
+
+	@LastModifiedBy
+	@Column(name = "updated_by")
+	private String updatedBy;
+
+	@Column(name = "deleted", nullable = false)
+	@lombok.Builder.Default
+	private boolean deleted = false;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	@Column(name = "deleted_by")
+	private String deletedBy;
+
+	@Version
+	@Column(name = "version")
+	private Long version;
+
+	/**
+	 * Soft delete this entity
+	 *
+	 * @param deletedBy username of the user who performed the deletion
+	 */
+	public void softDelete(String deletedBy) {
+		this.deleted = true;
+		this.deletedAt = LocalDateTime.now();
+		this.deletedBy = deletedBy;
+	}
+
+}
